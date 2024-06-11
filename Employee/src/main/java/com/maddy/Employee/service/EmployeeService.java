17a33dc0b108f43460d07ca8b93fb8cae14f0df6
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2024-06-11 21:38:48 +0530

    Add files via upload

---
+++ Employee/src/main/java/com/maddy/Employee/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.maddy.Employee.service;
+
+import com.maddy.Employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
