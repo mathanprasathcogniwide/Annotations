7b264525f61392a2431fd0fd5c7db093d4b52a47
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2024-07-05 12:45:28 +0530

    Delete Employee directory

--- Employee/src/main/java/com/maddy/Employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maddy.Employee.repository;
-
-import com.maddy.Employee.entity.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-}
